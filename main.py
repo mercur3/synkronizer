56ab16763206510935b5c5dab0acb71aff12f5ad
Andri Reveli <reveliandri@gmail.com>
2021-07-02 15:48:43 +0200

    created basic stucture for handling inputs

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import os
+import sys
 from pathlib import Path
+from options import Options
 
-USER_HOME = str(Path.home())                     # get $HOME
-CONFIG_PATH = USER_HOME + "/tmp/linux-configs"   # path to config folder
 
-items = list(filter(lambda x: x != ".git", os.listdir(CONFIG_PATH)))  # get all valid items
-for i in items:
-    print(i)   # TODO os.system(make a link to config folder inside the git repo)
+def main():
+    USER_HOME = str(Path.home())                     # get $HOME
+    CONFIG_PATH = USER_HOME + "/tmp/linux-configs"   # path to config folder
+
+    items = list(filter(lambda x: x != ".git", os.listdir(CONFIG_PATH)))  # get all valid items
+    for i in items:
+        print(i)   # TODO os.system(make a link to config folder inside the git repo)
+
+
+# TODO should return an Option
+def read_configs():
+    pass
+
+if __name__ == "__main__":
+    #main()
+    l = len(sys.argv)
+    if l > 1:
+        print(f"the lenght is {l}")
+        for i in range(1, l):
+            print(sys.argv[i])
+    read_configs()
---
+++ options.py
@@ -0,0 +1,6 @@
+class Options:
+    def __init__(self, home, config, verbose, requests_help):
+        self.home = home
+        self.config = config
+        self.verbose = verbose
+        self.requests_help = requests_help
