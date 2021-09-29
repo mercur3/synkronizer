fba247f4da383b6d1c008762609133657d5e1a64
Andri Reveli <reveliandri@gmail.com>
2021-09-29 16:40:28 +0200

    refactor some code

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,12 @@ pub struct App {
 	pub config: PathBuf,
 }
 
-static HOME_KEYWORD: &str = "home";
-static CONFIG_KEYWORD: &str = "config";
+const HOME_KEYWORD: &str = "home";
+const CONFIG_KEYWORD: &str = "config";
 
 impl App {
 	pub fn from_config_file(path: &Path) -> Self {
-		let (home, config) = crate::parse_file(path);
+		let (home, config) = App::parse_file(path);
 		if home.is_empty() {
 			panic!("No home");
 		}
@@ -27,45 +27,43 @@ impl App {
 
 		return App { home, config };
 	}
-}
 
-fn parse_file(path: &Path) -> (String, String) {
-	let mut home = String::default();
-	let mut config = String::default();
+	fn parse_file(path: &Path) -> (String, String) {
+		let mut home = String::default();
+		let mut config = String::default();
 
-	let file = io::BufReader::new(File::open(path).expect("cannot open file"));
-	for line in file.lines() {
-		let args = line.unwrap_or_default();
-		let args = args.trim();
-		if args.is_empty() || args.starts_with('#') {
-			continue;
+		let file = io::BufReader::new(File::open(path).expect("cannot open file"));
+		for line in file.lines() {
+			let args = line.unwrap_or_default();
+			let args = args.trim();
+			if args.is_empty() || args.starts_with('#') {
+				continue;
+			}
+
+			let index = args
+				.find('=')
+				.expect(format!("Missing `=`\nLine: {}", args).as_str());
+			let left = args[..index].trim().to_lowercase();
+			let right = args[index + 1..].trim().to_lowercase();
+
+			if left.is_empty() {
+				panic!("Missing left hand side");
+			}
+			if right.is_empty() {
+				panic!("Missing right hand side");
+			}
+
+			match left.as_str() {
+				crate::HOME_KEYWORD => home = right,
+				crate::CONFIG_KEYWORD => config = right,
+				_ => {
+					eprintln!("Keyword {} is not known", left);
+					eprintln!("Line: {}", args);
+					panic!("Unknown keyword");
+				}
+			}
 		}
 
-		let index = args
-			.find('=')
-			.expect(format!("Missing `=`\nLine: {}", args).as_str());
-		let left = args[..index].trim().to_lowercase();
-		let right = args[index + 1..].trim().to_lowercase();
-
-		if left.is_empty() {
-			panic!("Missing left hand side");
-		}
-		if right.is_empty() {
-			panic!("Missing right hand side");
-		}
-
-		if left == crate::HOME_KEYWORD {
-			home = right;
-		}
-		else if left == crate::CONFIG_KEYWORD {
-			config = right;
-		}
-		else {
-			eprintln!("Keyword {} is not known", left);
-			eprintln!("Line: {}", args);
-			panic!("Unknown keyword");
-		}
+		return (home, config);
 	}
-
-	return (home, config);
 }
