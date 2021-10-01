80d78b48b7911f07e55452e5d4c476ff843985cd
Andri Reveli <reveliandri@gmail.com>
2021-10-01 17:07:07 +0200

    future work

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use std::path::Path;
 use synkronizer::*;
 
 fn main() {
-	let app = App::from_config_file(Path::new("config.txt"));
+	// let app = App::from_config_file(Path::new("config.txt"));
 	// app.sync_home();
 	// app.sync_config();
+
+	// FIXME convert to unit test
+	// let src = &Path::new("/home/andri/code/synkronizer/tests/x/src");
+	// let target = &Path::new("/home/andri/code/synkronizer/tests/x/target");
+	// let resolve = &sync::ConflictResolver::Prompt;
+	// sync::sync(src, target, resolve);
 }
